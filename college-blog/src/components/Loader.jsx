ca8e7c8cc9e9c5632e7fcf33b12a3da0eaf120a3
DURGANJALI GADAMSETTY <147343685+G-DURGANJALI@users.noreply.github.com>
2025-04-18 17:05:51 +0530

    Delete college-blog directory

--- college-blog/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-export default function Loader() {
-	return (
-	  <div className="h-screen flex items-center justify-center bg-gray-100">
-		<div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid" />
-	  </div>
-	);
-  }
-  
